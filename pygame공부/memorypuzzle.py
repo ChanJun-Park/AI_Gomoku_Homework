34b0dab8427dae1f43281e83eb90e40e46a9f9b7
ChanJun-Park <pcj01142@gmail.com>
2019-05-10 20:28:13 +0900

    2019-05-10

--- pygame공부/memorypuzzle.py
+++ pygame공부/memorypuzzle.py
@@ -1,3 +1,7 @@
+
+
+
+
 import random, pygame, sys
 from pygame.locals import *
 
@@ -9,7 +13,7 @@ BOXSIZE = 40
 GAPSIZE = 10
 BOARDWIDTH = 10     # 아이콘 가로줄 수
 BOARDHEIGHT = 7     # 아이콘 세로줄 수
-assert (BOARDWIDTH * BOARDHEIGHT) % 2 == 0, 'Borad needs to have an even number of boxes for pairs of matches.'
+assert (BOARDWIDTH * BOARDHEIGHT) % 2 == 0, 'Board needs to have an even number of boxes for pairs of matches.'
 XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * (BOXSIZE + GAPSIZE))) / 2)
 YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * (BOXSIZE + GAPSIZE))) / 2)
 
@@ -58,6 +62,60 @@ def main():
 
     DISPLAYSURF.fill(BGCOLOR)
     startGameAnimation(mainBoard)
+
+    while True:  # 게임 루프
+        mouseClicked = False
+
+        DISPLAYSURF.fill(BGCOLOR) # 윈도우를 그린다
+        drawBoard(mainBoard, revealedBoxes)
+
+        for event in pygame.event.get():  # 이벤트 처리 루프
+            if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+            elif event.type == MOUSEMOTION:
+                mousex, mousey = event.pos
+            elif event.type == MOUSEBUTTONUP:
+                mousex, mousey = event.pos
+                mouseClicked = True
+
+        boxx, boxy = getBoxAtPixel(mousex, mousey)
+        if boxx != None and boxy != None:
+            # 마우스가 현재 박스 위에 있다.
+            if not revealedBoxes[boxx][boxy]:
+                drawHighlightBox(boxx, boxy)
+            if not revealedBoxes[boxx][boxy] and mouseClicked:
+                revealBoxesAnimation(mainBoard, [(boxx, boxy)])
+                revealedBoxes[boxx][boxy] = True  # 상자를 보이는 것으로 설정
+                if firstSelection == None:
+                    firstSelection = (boxx, boxy)
+                else: # 현재의 상자가 두 번째 클릭한 상자라면
+                    # 두 아이콘이 서로 맞는 짝인지 검사한다.
+                    icon1shape, icon1color = getShapeAndColor(mainBoard, firstSelection[0], firstSelection[1])
+                    icon2shape, icon2color = getShapeAndColor(mainBoard, boxx, boxy)
+                    if icon1shape != icon2shape or icon1color != icon2color:
+                        pygame.time.wait(1000)
+                        coverBoxesAnimation(mainBoard, [(firstSelection[0], firstSelection[1]), (boxx, boxy)])
+                        revealedBoxes[firstSelection[0]][firstSelection[1]] = False
+                        revealedBoxes[boxx][boxy] = False
+                    elif hasWon(revealedBoxes):  # 아이콘이 서로 짝이라면
+                        gameWonAnimation(mainBoard)
+                        pygame.time.wait(2000)
+
+                        # 게임판을 재설정한다.
+                        mainBoard = getRandomizedBoard()
+                        revealedBoxes = generateRevealedBoxesData(False)
+
+                        # 잠시 동안 게임판의 상자를 열어서 보여준다
+                        drawBoard(mainBoard, revealedBoxes)
+                        pygame.display.update()
+                        pygame.time.wait(1000)
+
+                        # 게임 시작 애니메이션을 보여준다
+                        startGameAnimation(mainBoard)
+                    firstSelection = None
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
     pass
 
 def generateRevealedBoxesData(val):
@@ -102,14 +160,63 @@ def leftTopCoordsOfBox(boxx, boxy):
     top = boxy * (BOXSIZE + GAPSIZE) + YMARGIN
     return (left, top)
 
+def getBoxAtPixel(x, y):
+    for boxx in range(BOARDWIDTH):
+        for boxy in range(BOARDHEIGHT):
+            left, top = leftTopCoordsOfBox(boxx, boxy)
+            boxRect = pygame.Rect(left, top, BOXSIZE, BOXSIZE)
+            if boxRect.collidepoint(x, y):
+                return (boxx, boxy)
+    return (None, None)
+
 def drawIcon(shape, color, boxx, boxy):
     quarter = int(BOXSIZE * 0.25) # syntatic sugar
     half =    int(BOXSIZE * 0.5)  # syntatic sugar
+
+    left, top = leftTopCoordsOfBox(boxx, boxy)  # 보드의 좌표에서 픽셀의 좌표를 얻는다.
+    # 형태를 그린다
+    if shape == DONUT:
+        pygame.draw.circle(DISPLAYSURF, color, (left + half, top + half), half - 5)
+        pygame.draw.circle(DISPLAYSURF, BGCOLOR, (left + half, top + half), quarter - 5)
+    elif shape == SQUARE:
+        pygame.draw.rect(DISPLAYSURF, BGCOLOR, (left + quarter, top + quarter, BOXSIZE - half, BOXSIZE - half))
+    elif shape == DIAMOND:
+        pygame.draw.polygon(DISPLAYSURF, color, ((left + half, top), (left + BOXSIZE - 1, top + half),\
+                                                 (left + half, top + BOXSIZE - 1), (left, top + half)))
+    elif shape == LINES:
+        for i in range(0, BOXSIZE, 4):
+            pygame.draw.line(DISPLAYSURF, color, (left, top + i), (left + i, top))
+            pygame.draw.line(DISPLAYSURF, color, (left + i, top + BOXSIZE - 1), (left + BOXSIZE -1, top + i))
+    elif shape == OVAL:
+        pygame.draw.ellipse(DISPLAYSURF, color, (left, top + quarter, BOXSIZE, half))
 
 def getShapeAndColor(board, boxx, boxy):
     # x,y 위치의 아이콘 형태의 값은 board[x][y][0]에 있다.
     # x,y 위치의 아이콘 색깔의 값은 board[x][y][1]에 있다.
     return board[boxx][boxy][0], board[boxx][boxy][1]
+
+def drawBoxCovers(board, boxes, coverage):
+    # 닫히거나 열린 상태의 상자를 그린다
+    # 상자는 아이템 2개짜리 리스트이며 상자의 x, y 위치를 가진다.
+    for box in boxes:
+        left, top = leftTopCoordsOfBox(box[0], box[1])
+        pygame.draw.rect(DISPLAYSURF, BGCOLOR, (left, top, BOXSIZE, BOXSIZE))
+        shape, color = getShapeAndColor(board, box[0], box[1])
+        drawIcon(shape, color, box[0], box[1])
+        if coverage > 0: # 닫힌 상태이면, 덮개만 그린다
+            pygame.draw.rect(DISPLAYSURF, BOXCOLOR, (left, top, coverage, BOXSIZE))
+    pygame.display.update()
+    FPSCLOCK.tick(FPS)
+
+def revealBoxesAnimation(board, boxesToReveal):
+    # 상자가 열리는 에니메이션 수행
+    for coverage in range(BOXSIZE, (-REVEALSPEED) -1, -REVEALSPEED):
+        drawBoxCovers(board, boxesToReveal, coverage)
+
+def coverBoxesAnimation(board, boxesToCover):
+    # 상자가 닫히는 에니메이션 수행
+    for coverage in range(0, BOXSIZE + REVEALSPEED, REVEALSPEED):
+        drawBoxCovers(board, boxesToCover, coverage)
 
 def drawBoard(board, revealed):
     # 모든 상자를 상태에 맞게 그리기
@@ -124,6 +231,10 @@ def drawBoard(board, revealed):
                 shape, color = getShapeAndColor(board, boxx, boxy)
                 drawIcon(shape, color, boxx, boxy)
 
+def drawHighlightBox(boxx, boxy):
+    left, top = leftTopCoordsOfBox(boxx, boxy)
+    pygame.draw.rect(DISPLAYSURF, HIGHLIGHTCOLOR, (left - 5, top - 5, BOXSIZE + 10, BOXSIZE + 10), 4)
+
 def startGameAnimation(board):
     # 무작위로 한 번에 8개씩 상자를 열어서 보여준다.
     coveredBoxes = generateRevealedBoxesData(False)
@@ -136,3 +247,5 @@ def startGameAnimation(board):
 
     drawBoard(board, coveredBoxes)
     for boxGroup in boxGroups:
+        revealBoxesAnimation(board, boxGroup)
+        coverBoxesAnimation(board, boxGroup)
